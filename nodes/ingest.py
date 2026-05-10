338174fca92962bee01d8e8981cfe1e820c88ceb
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-10 00:58:49 -0400

    feat: ingest node

---
+++ nodes/ingest.py
@@ -0,0 +1,38 @@
+from state import State, Ticket
+from main import data
+
+def ingest(state: State) -> dict:
+    """
+    Valida o JSON de entrada e normaliza o texto.
+    """
+    # Resgata sempre a primeira entrada do JSON, com a intenção de deletar ela depois
+    raw_ticket = data[0]
+    
+    # verificar se o chamado esta vazio
+    if not raw_ticket:
+        return {}
+    
+
+    # checar se todas os campos existem?
+    
+    # verificar se o free_text esta vazio (ja que todo o processamento depende dele)
+    if not raw_ticket["free_text"]:
+        return {}
+    
+    # Limpa o free_text
+    clean_text =  " ".join((raw_ticket["free_text"]).lower().strip().split())
+    
+    # mais metodos de limpar o texto?
+
+    # chamar uma LLM para arrumar erros ortográficos para melhorar o desempenho dos categorizadores?
+
+    # Atualiza o campo com o clean_text
+    normalized_text: Ticket = {
+        "id": raw_ticket["id"],
+        "timestamp": raw_ticket["timestamp"],
+        "channel": raw_ticket["channel"],
+        "requester_profile": raw_ticket["requester_profile"],
+        "free_text": clean_text
+    }
+    
+    return {"ticket": [normalized_text]}
