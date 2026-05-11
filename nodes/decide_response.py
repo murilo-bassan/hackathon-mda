c4820107864184dd41fed2f8457428f5c418cd0d
murilo_bassan <murilogabassan@gmail.com>
2026-05-11 10:12:54 -0400

    fix: fixing key names

--- nodes/decide_response.py
+++ nodes/decide_response.py
@@ -8,14 +8,15 @@ def decide_response(state: State) -> str:
     if state["prioridade_resultante"] <= 2 and state["categoria"] == "requisição":
         print("[decide_response] → draft_response")
         return "draft_response"
-    print("[decide_response] → emit (enfileirado para humano)")
-    return "emit"
+    print("[decide_response] → queue_only")
+    return "queue_only"
     """
-    prioridade = state["tickets"][state["current_ticket_index"]]["resulting_priority"]
-    categoria = state["tickets"][state["current_ticket_index"]]["category"].strip().lower()
+    partial = state.get("response", {})
+    prioridade = partial.get("resulting_priority", 99)
+    categoria  = partial.get("category", "").strip().lower()
 
     if prioridade <= 4 and categoria == "requisição":
         print("[decide_response] → draft_response")
         return "draft_response"
-    print("[decide_response] → emit (enfileirado para humano)") #queue_only
-    return "emit"
+    print("[decide_response] → queue_only")
+    return "queue_only"
