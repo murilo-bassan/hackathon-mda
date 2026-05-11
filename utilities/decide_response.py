b6fc19262d2c649d59cd2a2da3fdbace218b8e0f
murilo_bassan <murilogabassan@gmail.com>
2026-05-11 15:27:37 -0400

    fix: changing priority value

--- utilities/decide_response.py
+++ utilities/decide_response.py
@@ -1,11 +1,11 @@
 # Decide, após o nó `route`, se o chamado vai para draft_response ou direto para emit 
-from state import State
+from state.state import State
 
 def decide_response(state: State) -> str:
     """
     Retorna o nome do próximo nó com base nas regras de negócio.
     
-    if state["prioridade_resultante"] <= 2 and state["categoria"] == "requisição":
+    if state["prioridade_resultante"] <= 3 and state["categoria"] == "requisição":
         print("[decide_response] → draft_response")
         return "draft_response"
     print("[decide_response] → queue_only")
