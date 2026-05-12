93dcb1fb96fcd633e74022e9a78ffa55e0aa025e
murilo_bassan <murilogabassan@gmail.com>
2026-05-12 18:02:24 -0400

    refactor: adding ticket_id and department

--- nodes/classify_type.py
+++ nodes/classify_type.py
@@ -14,10 +14,11 @@ def classify_type(state: State) -> dict:
     
     # Retornamos a resposta formatada para o LangGraph
     partial = dict(state.get("response", {}))
+    partial["ticket_id"] = state["ticket"]["id"]
     partial["category"] = response_data.get("category", "Indefinida")
     partial["service_type"] = response_data.get("service_type", "Geral")
     partial["support_level"] = response_data.get("support_level", 1)
     partial["category_justification"] = response_data.get("category_justification", "Sem justificativa")
+    partial["department"] = response_data.get("department", "Geral")
     
     return {"response": partial}
-    
