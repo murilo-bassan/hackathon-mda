c011e67d376c8d55fd4133dd1de27ae96b17699c
murilo_bassan <murilogabassan@gmail.com>
2026-05-15 17:20:03 -0400

    feat: func draft

---
+++ utilities/decide_input.py
@@ -0,0 +1,10 @@
+from core.state.state import State
+
+def decide_input(state: State) -> dict:
+    """
+    Decide which input node to execute based on the state of the system.
+    """
+    if state.get("input_type") == "incident":
+        return {"node": "ingest_incident"}
+    else:
+        return {"node": "ingest"}
