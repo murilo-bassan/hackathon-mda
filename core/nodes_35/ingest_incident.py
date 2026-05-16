b0df105022ece9539c7cfe6112509568cacf7705
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-15 20:28:30 -0400

    feat: ingest_incident node & ingest_incident_ticket

--- core/nodes_35/ingest_incident.py
+++ core/nodes_35/ingest_incident.py
@@ -1,5 +1,25 @@
 from core.state.state import State
+from utilities.ingest_incident_ticket import IncidentTicket
+from pydantic import ValidationError
+
+import uuid
 
 def ingest_incident(state: State) -> dict:
 
-    return {}
+    incident = state.get("incident")
+    incident["id"] = str(uuid.uuid4())
+
+    try:
+        # Validando e processando os dados
+        validated_incident = IncidentTicket.model_validate(incident)
+    
+        # Atualiza os campos com os dados validados
+        normalized_incident = validated_incident.model_dump(mode='json')
+
+    # Caso aconteça algum erro de validação
+    except ValidationError as error:
+        incident["alert_draft"] = error.json()
+
+    return {
+        "incident": normalized_incident
+    }
---
+++ utilities/ingest_incident_ticket.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+class IncidentTicket(BaseModel):
+    id: str = Field(
+        description="Identificador único do chamado"
+    )
+    
+    timestamp: datetime = Field(
+        description="Data e hora do chamado"
+    )
+    
+    report_text: str = Field(
+        min_length=2, 
+        description="Texto digitado pelo usuário"
+    )
