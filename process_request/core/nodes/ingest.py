5affd19d3be900aa270a018c1b7fbe34d9bc9db4
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-16 19:13:37 -0400

    fix: id failsafe

--- process_request/core/nodes/ingest.py
+++ process_request/core/nodes/ingest.py
@@ -3,22 +3,28 @@ from process_request.core.state.request_state import State
 from process_request.utilities.ingest_ticket import IngestTicket
 from pydantic import ValidationError
 
+import uuid
+
 def ingest(state: State) -> dict:
     """
     Valida o JSON de entrada e normaliza o texto.
     """
     # Resgata a entrada do JSON
-    raw_ticket = state.get("ticket")
+    raw_ticket = dict(state.get("ticket") or {})
 
     partial = dict(state.get("response", {}))
-    partial["validation_status"] = True #default validation_status
 
     try:
+        if (not raw_ticket.get("id")):
+            raw_ticket["id"] = str(uuid.uuid4())
+        
         # Validando e processando os dados
         validated_ticket = IngestTicket.model_validate(raw_ticket)
     
         # Atualiza os campos com os dados validados
         normalized_ticket = validated_ticket.model_dump(mode='json')
+
+        partial["validation_status"] = True #default validation_status
 
     # Caso aconteça algum erro de validação
     except ValidationError as error:
