d9aa38baa4ada59b58aeb3e0e79fd4523902228b
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-17 18:46:04 -0400

    refactor: changing subgraph logic in graph builder

--- general_process/core/graph_builder.py
+++ general_process/core/graph_builder.py
@@ -3,28 +3,24 @@ from langgraph.graph import END, START, StateGraph
 from general_process.core.nodes.prepare_incident import prepare_incident
 from general_process.core.nodes.classify_input import classify_input
 from general_process.core.nodes.normalize_input import normalize_input
+from general_process.core.nodes.request_workflow import request_workflow
+from general_process.core.nodes.incident_workflow import incident_workflow
+
 from general_process.core.state.state import State
 from general_process.utilities.decide_input import decide_input
-from process_request.core.subgraph_request_builder import build_request_subgraph
-from process_incident.core.subgraph_incident_builder import build_incident_subgraph
 
 
 def build_graph():
 
-    request_subgraph = build_request_subgraph()
-    incident_subgraph = build_incident_subgraph()
-
     builder = StateGraph(State)
-    builder.add_node("prepare_incident", prepare_incident) 
-
 
     # Registro de nós gerais
     builder.add_node("normalize_input", normalize_input)
     builder.add_node("classify_input", classify_input)
 
     # Subgraphs
-    builder.add_node("request_workflow", request_subgraph)
-    builder.add_node("incident_workflow", incident_subgraph)
+    builder.add_node("request_workflow", request_workflow)
+    builder.add_node("incident_workflow", incident_workflow)
 
     # Arestas normais (fluxo sequencial principal)
     builder.add_edge(START, "normalize_input")
@@ -35,12 +31,11 @@ def build_graph():
         decide_input,
         {
             "request": "request_workflow",
-            "incident": "prepare_incident",
+            "incident": "incident_workflow",
         }
     )
 
     builder.add_edge("request_workflow", END)
-    builder.add_edge("prepare_incident", "incident_workflow")
     builder.add_edge("incident_workflow", END)
 
     return builder.compile()
