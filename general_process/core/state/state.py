2cc8e6a27c3a9d2df9692072a5027a32a031e44b
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-17 18:47:36 -0400

    fix: removing keys from state

--- general_process/core/state/state.py
+++ general_process/core/state/state.py
@@ -1,13 +1,7 @@
-from typing import TypedDict, Literal, NotRequired
+from typing import TypedDict, Literal
 
 class State(TypedDict, total=False):
     raw_input: dict
     input_type: Literal["request", "incident"]
     input_justification: str
-    input_text: str
     result: dict
-    ticket: dict
-    response: dict
-    closing_message: str
-    incident: dict
-    email: dict
