2b2fd50a88a763e3be2d6c1351b0d3790eaff096
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-17 18:48:42 -0400

    refactor: adding closing_mesage to emit

--- process_request/core/nodes/emit.py
+++ process_request/core/nodes/emit.py
@@ -9,14 +9,20 @@ from general_process.utilities.logger_config import setup_logger
 logger = setup_logger(__name__)
 
 def emit(state: State) -> dict:
-    ticket = state["ticket"]
+    ticket = state.get("ticket", {})
     partial = state.get("response", {})
 
     os.makedirs(RESPONSES_PATH, exist_ok=True)
 
+    closing = (
+        "Seu chamado foi encerrado pela equipe da AGETIC/UFMS. "
+        "Caso o problema persista, reabra o chamado ou entre em contato: "
+        "suporte.agetic@ufms.br | (67) 3345-7292."
+    )
+
     response = {
-        "ticket_id": ticket["id"],
-        "category": partial.get("category"),
+        "ticket_id": ticket.get("id", ""),
+        "category": partial.get("category", ""),
         "urgency": partial.get("urgency", 0),
         "impact": partial.get("impact", 0),
         "resulting_priority": partial.get("resulting_priority", 0),
@@ -26,16 +32,17 @@ def emit(state: State) -> dict:
         "category_justification": partial.get("category_justification", ""),
         "department": partial.get("department", ""),
         "response_draft": partial.get("response_draft", ""),
+        "closing_message": closing,
         "needs_more_info": ticket.get("needs_more_info", False),
         "info_justification": ticket.get("info_justification", ""),
         "validation_status": partial.get("validation_status", "pending")
     }
 
     # Substituindo os prints poluídos pelo logger
-    logger.info(f"Emitindo resultados para o Ticket {ticket['id']}")
+    logger.info(f"Emitindo resultados para o Ticket {ticket.get("id")}")
 
     # SALVAR JSON INDIVIDUAL
-    with open(RESPONSES_PATH / f"ticket_{ticket['id']}.json", "w", encoding="utf-8") as f:
+    with open(RESPONSES_PATH / f"ticket_{ticket.get("id")}.json", "w", encoding="utf-8") as f:
         json.dump(response, f, ensure_ascii=False, indent=2)
 
     # GERAR / ATUALIZAR CSV
@@ -48,14 +55,7 @@ def emit(state: State) -> dict:
             writer.writeheader()
 
         writer.writerow(response)
-    
-    closing = (
-        "Seu chamado foi encerrado pela equipe da AGETIC/UFMS. "
-        "Caso o problema persista, reabra o chamado ou entre em contato: "
-        "suporte.agetic@ufms.br | (67) 3345-7292."
-    )
 
     return {
         "response": response,
-        "closing_message": closing,
     }
