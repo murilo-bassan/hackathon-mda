dee2bd0d184dc0dd1768d10a0a94f0522ace74a8
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-18 02:15:43 -0400

    refactor: adding datasetAnalyzer func to main_incident.py

--- main_incident.py
+++ main_incident.py
@@ -1,10 +1,11 @@
 from main import main
 from process_incident.utilities.accuracy import run_accuracy
 from process_incident.utilities.config import DATA_PATH
-
+from process_incident.utilities.dataset_analyzer import datasetAnalyzer
 
 if __name__ == "__main__":
+    datasetAnalyzer()
+    
     main(DATA_PATH)
-
 
     run_accuracy()
