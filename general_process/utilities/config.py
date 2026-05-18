52254cad832b20a3267e32dce0beff63ad815228
Paola Campos da Silva <paa.campossilva@gmail.com>
2026-05-17 20:36:11 -0400

    refactor: all logs will be in general_process/artifacts/logs/execucao.log

--- general_process/utilities/config.py
+++ general_process/utilities/config.py
@@ -9,7 +9,7 @@ CLASSIFY_INPUT_PROMPT_PATH = PROMPTS_PATH / "classify_input_prompt.md"
 REQUEST_BASE_DIR = ROOT_DIR / "process_request"
 INCIDENT_BASE_DIR = ROOT_DIR / "process_incident"
 
-ARTIFACT_PATH = REQUEST_BASE_DIR / "artifacts"
+ARTIFACT_PATH = BASE_DIR / "artifacts"
 DOC_PATH = ROOT_DIR / "docs"
 LOG_PATH = ARTIFACT_PATH / "logs"
 GRAPH_PNG = DOC_PATH / "graph.png"
